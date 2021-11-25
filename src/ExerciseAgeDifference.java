9e2eada49dfe3633af1121a007f4787a9ab4dc29
William Suane <william.suane@devdojo.academy>
2021-11-25 14:35:06 +0100

    docs: add exercise explanation

--- src/ExerciseAgeDifference.java
+++ src/ExerciseAgeDifference.java
@@ -1,3 +1,11 @@
+/*
+Create a class called ExerciseAgeDifference
+Create a variable that can store the first age and assign some value
+Create a second variable to store the second age and also assign some value
+Create a third variable that will be the difference between first and second age
+Print in the console all the ages and the difference
+*/
+
 public class ExerciseAgeDifference {
     public static void main(String[] args) {
         int firstAge = 40;
