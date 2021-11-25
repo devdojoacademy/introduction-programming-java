a684cd3a4aa355c5237d99eeb22654b1d2746169
William Suane <william.suane@devdojo.academy>
2021-11-25 15:50:45 +0100

    feat: video14

---
+++ src/Conditionals01.java
@@ -0,0 +1,11 @@
+public class Conditionals01 {
+    public static void main(String[] args) {
+        boolean condition = true;
+        if(condition){
+            System.out.println("Inside IF statement");
+        }else {
+            System.out.println("Inside ELSE");
+        }
+        System.out.println("Outside IF-ELSE");
+    }
+}
