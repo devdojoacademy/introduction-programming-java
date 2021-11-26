8fc1ab4affa36831afbaae8bab31b9952b8a12fd
William Suane <william.suane@devdojo.academy>
2021-11-26 13:09:13 +0100

    feat: video16

---
+++ src/Conditionals03.java
@@ -0,0 +1,14 @@
+public class Conditionals03 {
+    public static void main(String[] args) {
+        int firstAge = 20;
+        int secondAge = 22;
+        // if firstAge >= secondAge: first age is greater or equal
+        // else firstAge is not greater or equal
+        boolean isFirstAgeGreaterOrEqualThanSecondAge = firstAge >= secondAge;
+        if(firstAge >= secondAge){
+            System.out.println("first age is greater or equal second age");
+        }else{
+            System.out.println("first age is not greater or equal second age");
+        }
+    }
+}
