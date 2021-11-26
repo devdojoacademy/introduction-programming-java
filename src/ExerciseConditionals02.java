b3c1c4aebe775ac9cb440f9f57d36fba9944c27e
William Suane <william.suane@devdojo.academy>
2021-11-26 14:31:39 +0100

    feat: video19

---
+++ src/ExerciseConditionals02.java
@@ -0,0 +1,15 @@
+/*
+Given a number
+If the number is even print the message: The number: <number> is even
+Else print the message The number: <number> is odd
+*/
+public class ExerciseConditionals02 {
+    public static void main(String[] args) {
+        int number = 1;
+        if((number % 2) == 0){
+            System.out.println("The number: "+number+" is even");
+        }else{
+            System.out.println("The number: "+number+" is odd");
+        }
+    }
+}
