fd5b327e4f471e2d3b9cc50783ee4cc884818ac3
William Suane <william.suane@devdojo.academy>
2021-11-29 15:25:13 +0100

    feat: video24

--- src/Conditionals06.java
+++ src/Conditionals06.java
@@ -2,9 +2,9 @@ public class Conditionals06 {
     public static void main(String[] args) {
        float ps5Price = 500;
        double creditCardAvailableCredit = 5000;
-       double checkingAccountValue = 400;
-       double savingsAccountValue = 200;
-       if(creditCardAvailableCredit > ps5Price || checkingAccountValue > ps5Price || savingsAccountValue > ps5Price){
+       double checkingAccountBalance = 400;
+       double savingsAccountBalance = 200;
+       if(creditCardAvailableCredit > ps5Price || checkingAccountBalance > ps5Price || savingsAccountBalance > ps5Price){
            System.out.println("I can buy a PS5");
        }else {
            System.out.println("Let's buy next month");
---
+++ src/Conditionals07.java
@@ -0,0 +1,16 @@
+public class Conditionals07 {
+    public static void main(String[] args) {
+        float income = 12000.0F;
+        //$9,951 to $40,525
+        // $40,526 to $86,375
+        if(income <= 9550){
+            System.out.println("10% = "+ (income * 0.1));
+        }else if(income >= 9551 && income <= 40525){
+            System.out.println("12% = "+ (income * 0.12));
+        }else if(income >= 40526 && income <= 86375) {
+            System.out.println("22% = "+ (income * 0.22));
+        }else {
+            System.out.println("30% = "+ (income * 0.30));
+        }
+    }
+}
