684d8001e825cf897cd3ac21576c4c9cd334b7cc
William Suane <william.suane@devdojo.academy>
2021-11-29 15:57:21 +0100

    feat: video26

---
+++ src/InputData01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class InputData01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, type your name:");
+        String name = input.next();
+        System.out.println("Now, type your age");
+        int age = input.nextInt();
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
