c4c2e1ee6fee2ae11ba0b7986e57a9c08cb3be81
William Suane <william.suane@devdojo.academy>
2021-12-01 15:35:55 +0100

    feat: video34

---
+++ src/academy/devdojo/loops/While01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.loops;
+
+public class While01 {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter < 10){
+            System.out.println(counter + " HEEEEEEEY DEVDOJO!");
+            counter = counter + 1;
+        }
+    }
+}
+
+// [ 1 ] = 0 + 1
+// [ 2 ] = 1 + 1
+// [ 3 ] = 2 + 1
