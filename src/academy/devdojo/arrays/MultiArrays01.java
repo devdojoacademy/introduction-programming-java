90c6ce531067d152cee56ec00f9e045e7a2f83e2
William Suane <william.suane@devdojo.academy>
2021-12-03 15:59:00 +0100

    feat: video48

---
+++ src/academy/devdojo/arrays/MultiArrays01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.arrays;
+
+public class MultiArrays01 {
+    public static void main(String[] args) {
+        int[][] array1 = new int[2][3];
+        array1[0][0] = 1;
+        array1[0][1] = 2;
+        array1[0][2] = 3;
+        array1[1][0] = 4;
+        array1[1][1] = 5;
+        array1[1][2] = 6;
+        System.out.println(array1[0][0]);
+        System.out.println(array1[0][1]);
+        System.out.println(array1[0][2]);
+        System.out.println(array1[1][0]);
+        System.out.println(array1[1][1]);
+        System.out.println(array1[1][2]);
+
+    }
+}
